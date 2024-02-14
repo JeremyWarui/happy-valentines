1e806ba2bd6154a5803f34a9eef0293d97d58857
JeremyWarui <69347073+JeremyWarui@users.noreply.github.com>
2024-02-14 13:55:45 +0000

    Happy valentines

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outputDir: 'dist' // or 'public' or any other name
+  },
   test: {
     globals: true,
     environment: 'jsdom',
